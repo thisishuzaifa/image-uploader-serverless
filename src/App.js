7216261eddf1119a5d54ec7ff5367da657438712
Huzaifa Khalid <huzaifa_khalid97@outlook.com>
2022-03-02 03:12:53 -0800

    set up static site on s3 bucket

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import {useState, useEffect} from 'react'
+
 
 function App() {
+
+const [file, setFile] = useState();
+const [title, setTitle] = useState("")
+const [images, setImages] = useState([])
+useEffect(() => {
+  async function getTitle() {
+    const titleResult = await axios.get('https://2r59gvz6q4.execute-api.ca-central-1.amazonaws.com/Dev/appdetails')
+    const message = titleResult.data.message
+    setTitle(message)
+
+    const imagesResult = await axios.get('https://2r59gvz6q4.execute-api.ca-central-1.amazonaws.com/Dev/images')
+    const images = imagesResult.data.images
+    setImages(images)
+
+
+  }
+  getTitle()
+}, [])
+
+const submit = async event => {
+  event.preventDefault();
+
+  const urlResult = await axios.get('https://2r59gvz6q4.execute-api.ca-central-1.amazonaws.com/Dev/signedurl')
+  const url = urlResult.data.url
+  await axios.put(url, images, {headers: {'Content-Type': 'file.type'}})
+  const imageUrl = url.split("?")[0]
+  setImages([...images, imageUrl])
+
+
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Serverless React App</h1>
+    <h2>{title}</h2>
+
+    <form onSubmit={submit}>
+      <input onChange={e => setFile(e.target.files[0])} type="file" accept="image/*"></input>
+      <button type="submit">Submit</button>
+    </form>
+    {
+      images.map(image => (
+      <img key={image} src={image} alt=""/>
+      ))
+
+    }
+
     </div>
   );
+
+
 }
 
 export default App;
